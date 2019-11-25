3684f653845aefc9c375c33c06de80e30564afa0
swdecker <swdecker@gmail.com>
2019-11-25 10:49:51 -0600

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,16 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+for(let i=1; i<arr.length;i++){
+  for(let inner = i-1; inner>=0 ;inner--){
+    if(arr[inner]>arr[inner+1] ){
+      
+      let oldInner = arr[inner]
+      let oldI =arr[inner+1]
+      arr[inner+1]=oldInner
+      arr[inner]=oldI
+    } else {break}
+  }
+}
+ return arr
 }
