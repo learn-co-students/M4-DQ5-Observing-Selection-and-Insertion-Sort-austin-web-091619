5254f8eff19c61c695edbada1a5c9478823ab46c
Joellehelm <joellenhelm@gmail.com>
2019-11-25 10:48:43 -0600

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,20 @@
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+ // iterate through arr
+ // compare number from first loop to number from second loop
+ // if number from second loop is smaller place it in the front of the array
+
+    for(let i = 0; i < arr.length; i++){
+      for(let a = i + 1; a < arr.length; a++){
+        if (arr[a] < arr[i]){
+          const first = arr[i]
+          const second = arr[a]
+
+          arr[i] = second
+          arr[a] = first
+
+
+        }
+      }
+    }
+    return arr
 }
